53eb3e6c97b12b545c37f1431771f8edc10388d8
Aquel32 <piotrc082@gmail.com>
2026-05-12 12:38:53 +0200

    Basic 3D SDF

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,85 @@
 // oxlint-disable-next-line no-unassigned-import
 import "./style.css";
-import tgpu, { common, d } from "typegpu";
+import tgpu, { common, d, std } from "typegpu";
+import * as sdf from '@typegpu/sdf';
+import { PrepareUI } from "./ui-controls";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<canvas id="canvas" width="256" height="256"></canvas>
-`;
 
 const root = await tgpu.init();
 
+export const boxPositionUniform = root.createUniform(d.vec3f);
+export const diskPositionUniform = root.createUniform(d.vec3f);
+
+PrepareUI();
+
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 const context = root.configureContext({ canvas });
+
+function sceneSdf(p: d.v3f) {
+  "use gpu";
+  const box = sdf.sdBox3d(p - boxPositionUniform.$, d.vec3f(0.12));
+  const disk = sdf.sdSphere(p - diskPositionUniform.$, 0.1);
+
+  return sdf.opSmoothDifference(box, disk, 0.1);
+}
+
+function normalAt(p: d.v3f) {
+  'use gpu';
+  const e = 0.002;
+  return std.normalize(d.vec3f(
+    sceneSdf(p + d.vec3f(e, 0, 0)) - sceneSdf(p - d.vec3f(e, 0, 0)),
+    sceneSdf(p + d.vec3f(0, e, 0)) - sceneSdf(p - d.vec3f(0, e, 0)),
+    sceneSdf(p + d.vec3f(0, 0, e)) - sceneSdf(p - d.vec3f(0, 0, e)),
+  ));
+};
+
+function march(ro: d.v3f, rd: d.v3f) {
+  'use gpu';
+  let t = d.f32(0);
+  let hit = d.f32(0);
+
+  for (let i = 0; i < 96; i++) {
+    const dist = sceneSdf(ro + rd * t);
+    if (dist < 0.002) {
+      hit = 1;
+      break;
+    }
+    t += dist;
+    if (t > 6) {
+      break;
+    }
+  }
+
+  return d.vec2f(t, hit);
+}
 
 const pipeline = root.createRenderPipeline({
   vertex: common.fullScreenTriangle,
   fragment: ({ uv }) => {
     "use gpu";
-    return d.vec4f(uv, 0, 1);
+
+    const screen = uv * 2 - 1; // -1 to 1
+    const ro = d.vec3f(0, 0, -1); // ray origin
+    const rd = std.normalize(d.vec3f(screen, 1.25)); // ray direction
+    const result = march(ro, rd); // x = distance, y = hit
+
+    if (result.y < 1) { // ray didnt hit
+      return d.vec4f(0, 0, 0, 1); // bg
+    }
+
+    const p = ro + rd * result.x; // hit point
+    const normal = normalAt(p);
+    const lightDir = std.normalize(d.vec3f(-0.35, 0.7, -0.55));
+    const diffuse = 0.2 + std.max(std.dot(normal, lightDir), 0) * 0.8;
+
+    return d.vec4f(1, 0, 0, 1) * diffuse;
   },
 });
 
-pipeline.withColorAttachment({ view: context }).draw(3);
+function render() {
+  pipeline.withColorAttachment({ view: context }).draw(3);
+
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
---
+++ src/ui-controls.ts
@@ -0,0 +1,32 @@
+import { d } from "typegpu";
+import { boxPositionUniform, diskPositionUniform } from "./main";
+
+export function PrepareUI() {
+    document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <canvas id="canvas" width="1920" height="1920"></canvas>
+
+    <div>
+    <label>Box X: <input id="boxX" type="range" min="-1" max="1" step="0.01" value="0"></label>
+    <label>Box Y: <input id="boxY" type="range" min="-1" max="1" step="0.01" value="0"></label>
+
+    <label>Disk X: <input id="diskX" type="range" min="-1" max="1" step="0.01" value="0"></label>
+    <label>Disk Y: <input id="diskY" type="range" min="-1" max="1" step="0.01" value="0"></label>
+    </div>
+    `;
+
+    document.querySelectorAll<HTMLInputElement>("input").forEach(input => {
+        input.addEventListener("input", () => {
+            updateUniforms();
+        });
+    });
+
+    function updateUniforms() {
+        const boxX = parseFloat((document.querySelector<HTMLInputElement>("#boxX")!).value);
+        const boxY = parseFloat((document.querySelector<HTMLInputElement>("#boxY")!).value);
+        const diskX = parseFloat((document.querySelector<HTMLInputElement>("#diskX")!).value);
+        const diskY = parseFloat((document.querySelector<HTMLInputElement>("#diskY")!).value);
+
+        boxPositionUniform.write(d.vec3f(boxX, boxY, 0));
+        diskPositionUniform.write(d.vec3f(diskX, diskY, 0));
+    }
+}
